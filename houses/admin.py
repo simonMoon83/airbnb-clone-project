e3b68cfb974c1e5660761187a74c8cb760f3ec32
simonMoon83 <munduki@gmail.com>
2023-10-04 21:01:53 +0900

    User App Custom Model setting

--- houses/admin.py
+++ houses/admin.py
@@ -6,5 +6,3 @@ from .models import House
 class HouseAdmin(admin.ModelAdmin):
     list_display = ("name", "price_per_night", "address", "pets_allowed")
     list_filter = ("price_per_night", "pets_allowed")
-    search_fields = ("address",)
-    # list_editable = ("price_per_night", "pets_allowed")
--- houses/models.py
+++ houses/models.py
@@ -13,3 +13,4 @@ class House(models.Model):
 
     def __str__(self):
         return self.name
+    
