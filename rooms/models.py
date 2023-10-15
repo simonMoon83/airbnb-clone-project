c4c3fd4becfe8cf4006e71e7ed67fef0faab9a05
simonMoon83 <munduki@gmail.com>
2023-10-15 20:48:56 +0900

    POWER ADMIN Method, Search Field, Admin Actions, Custom Filter

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,4 @@
+from audioop import avg
 from django.db import models
 from common.models import CommonModel
 
@@ -59,6 +60,16 @@ class Room(CommonModel):
 
     def total_amenities(self):
         return self.amenities.count()
+
+    def rating(room):
+        count = room.reviews.count()
+        if count == 0:
+            return "No Reviews"
+        else:
+            total_rating = 0
+            for review in room.reviews.all().values("rating"):
+                total_rating += review["rating"]
+            return round(total_rating / count, 2)
 
 
 class Amenity(CommonModel):
