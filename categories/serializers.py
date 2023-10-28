e95703bf8a5939663ff599caf24bc5af5544fc79
simonMoon83 <munduki@gmail.com>
2023-10-28 14:34:53 +0900

    9.12 ModelViewSet

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(serializers.Serializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
