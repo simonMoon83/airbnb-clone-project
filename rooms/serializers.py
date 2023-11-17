b14da5a25a90e14e7b8c627540e9ecbe9781d4e4
simonMoon83 <munduki@gmail.com>
2023-11-17 18:21:33 +0900

    10.9 Pagination

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
-from reviews.serializers import ReviewSerializer
 from categories.serializers import CategorySerializer
 
 
@@ -16,19 +15,11 @@ class AmenitySerializer(serializers.ModelSerializer):
 
 class RoomDetailSerializer(serializers.ModelSerializer):
     owner = TinyUserSerializer(read_only=True)
-    amenities = AmenitySerializer(
-        read_only=True,
-        many=True,
-    )
     category = CategorySerializer(
         read_only=True,
     )
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
-    reviews = ReviewSerializer(
-        many=True,
-        read_only=True,
-    )
 
     class Meta:
         model = Room
