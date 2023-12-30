d97cbd5745dac711a59b76dd81fc687740a2d653
simonMoon83 <munduki@gmail.com>
2023-12-30 21:17:00 +0900

    CORS_ALLOW_CREDENTIANLS ADD

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ MEDIA_ROOT = "uploads"
 
 MEDIA_URL = "user-uploads/"
 
-PAGE_SIZE = 3
+PAGE_SIZE = 4
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
@@ -165,4 +165,9 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
